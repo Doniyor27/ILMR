195d77c96beed7640fb6f93901024d23500bd47d
FastDelivery1 <doni404error@gmail.com>
2022-02-04 10:05:28 +0500

    header is reedited

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from './components/header'
 import Home from './page/home'
 import Footer from './components/footer'
 
 function App() {
   return (
     <div>
+      <Header/>
       <Home/>
       <Footer/>
     </div>
