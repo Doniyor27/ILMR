8f211a4730bc9852a1a7419eca3fd5c3cfd87084
Mavlon dev <mavlonweb@gmail.com>
2022-02-10 12:25:00 +0500

    first commit all heroes

--- src/components/SidebarLanguage.jsx
+++ src/components/SidebarLanguage.jsx
@@ -7,8 +7,8 @@ const SidebarLanguage = () => {
 	<div className="sidebar__language" onClick={() => setLangToggle(!langToggle)} >
 		<span>UZB</span>
 			<div className={`${langToggle ? 'language__options lang__show': 'language__options'}`}>
-				<div class="language__option">ENG</div>
-				<div class="language__option">RUS</div>
+				<div className="language__option">ENG</div>
+				<div className="language__option">RUS</div>
 	  		</div>
 	</div>
   )
--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -4,11 +4,25 @@ import Hero2 from '../../components/hero2';
 import Development from './components/development';
 import Hero from './components/hero';
 import HeroModal from './components/hero/components/HeroModal';
+import { heroData10, heroData11, heroData12, heroData13, heroData14, heroData15, heroData2, heroData3, heroData5, heroData6, heroData7, heroData8, heroData9 } from '../../components/heroData';
 
 const Homepage = () => {
   return (
 	  <div>
-		  <Hero2/>
+		  <Hero2 data={heroData2} index={2}/>
+		  {/* <Hero2 data={heroData3} index={1}/> */}
+		  {/* <Hero2 data={heroData3} index={2}/> */}
+		  {/* <Hero2 data={heroData5} index={1}/> */}
+		  {/* <Hero2 data={heroData6} index={3}/> */}
+		  {/* <Hero2 data={heroData7} index={2}/> */}
+		  {/* <Hero2 data={heroData8} index={4}/> */}
+		  {/* <Hero2 data={heroData9} index={2}/> */}
+		  {/* <Hero2 data={heroData10} index={4}/> */}
+		  {/* <Hero2 data={heroData11} index={6}/> */}
+		  {/* <Hero2 data={heroData12} index={2}/> */}
+		  {/* <Hero2 data={heroData13}/> ishlamayapti  */}
+		  {/* <Hero2 data={heroData14} index={3}/> */}
+		  {/* <Hero2 data={heroData15} index={3}/> */}
 		  {/* <Hero/>	 */}
 		  {/* <HeroModal/> */}
 		  {/* <Development/> */}
